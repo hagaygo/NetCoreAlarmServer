2cab257011596b7945c5744373ff29f613022f75
Hagay Goshen <hagay.goshen@gmail.com>
2019-10-28 13:42:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace NetCoreAlarmServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Missing parameters : ");
+                Console.WriteLine("1.Address to bind/listen");
+                Console.WriteLine("2.TCP Port");
+                Console.WriteLine("3.config.xml path");
+                return;
+            }
+            var ip = args[0];
+            var port = Convert.ToInt32(args[1]);
+            var configFilename = args[2];
+
+            var t = new Thread(delegate ()
+            {
+                // replace the IP with your system IP Address...
+                Server myserver = new Server(ip, port);
+            });
+            t.Start();
+
+            Console.WriteLine($"Alarm Server Started (on {ip}:{port})");
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace NetCoreAlarmServer
+{
+    class Server
+    {
+        TcpListener server = null;
+        public Server(string ip, int port)
+        {
+            IPAddress localAddr = IPAddress.Parse(ip);
+            server = new TcpListener(localAddr, port);
+            server.Start();
+            StartListener();
+        }
+
+        public void StartListener()
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    var t = new Thread(new ParameterizedThreadStart(HandleDeivce));
+                    t.Start(client);
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+                server.Stop();
+            }
+        }
+
+        public void HandleDeivce(Object obj)
+        {
+            var client = (TcpClient)obj;
+            var stream = client.GetStream();
+            
+            try
+            {
+                var sr = new StreamReader(stream);
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    Console.WriteLine("Got " + line);
+                    if (line.Contains("{"))
+                    {
+                        line = line.Substring(line.IndexOf("{")).Trim();
+                        Console.WriteLine("line after trim " + line);
+                    }                    
+                }
+                Console.WriteLine("Disconnected");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.ToString());
+                client.Close();
+            }
+        }
+    }
+}
